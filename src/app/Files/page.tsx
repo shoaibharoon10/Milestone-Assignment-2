be3115c0fb913963ccfaffc878e9677a99397cc8
Muhammmad Shoaib <165128079+shoaibharoon10@users.noreply.github.com>
2024-11-13 00:13:19 +0500

    Update page.tsx

--- src/app/Files/page.tsx
+++ src/app/Files/page.tsx
@@ -1,8 +1,8 @@
 export default function Files() {
     return (
       <div>
-        <h1>
-            Files Page
+        <h1 className="text-lg text-justify p-5 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, autem. Dolorum, iure nulla, earum error perferendis inventore magni dicta tempore vitae cum ipsam optio deserunt eaque saepe modi vero numquam.
         </h1>              
      </div>
     )
