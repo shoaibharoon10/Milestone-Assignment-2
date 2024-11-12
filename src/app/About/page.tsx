f41fe39dd50bf78a18ffbdaaba140ec102d7f186
Muhammmad Shoaib <165128079+shoaibharoon10@users.noreply.github.com>
2024-11-13 00:11:56 +0500

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
       <div>
-        <h1 className="pl-2">
+        <h1 className="text-lg text-justify p-5 ">
             This is my First Assignment of Next.Js project Milestone-1
         </h1>              
      </div>
