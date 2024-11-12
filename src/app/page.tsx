0504963326eb445590030e28883a1f1114030bd1
Muhammmad Shoaib <165128079+shoaibharoon10@users.noreply.github.com>
2024-11-12 23:52:18 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Home Page
-    </div>
-  )
+<div>
+<h1 className="text-yellow-100 border-slate-900 border-8 rounded-2xl pl-10 m-2
+   font-extrabold font-serif text-5xl text-center"> Home Page</h1>
+<p className="text-lg text-justify p-5 "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis aliquid ducimus nemo, suscipit praesentium veritatis delectus nesciunt! Odit corrupti eum, sit explicabo aliquam, necessitatibus laboriosam fugiat veritatis id, possimus asperiores?
+ Aliquam perspiciatis amet fugit, non dignissimos possimus cum repellat, temporibus modi, accusamus rem eligendi? Rem odio iure atque commodi deserunt molestiae neque, accusantium nihil perferendis expedita quas laboriosam nemo assumenda!
+ Tempore vel officia inventore voluptate ex. Eius libero nesciunt deleniti, sunt provident animi non ab unde odio et assumenda. Quia, autem velit est facere provident temporibus accusamus explicabo iste nam?
+ Voluptatibus voluptate quo expedita maxime aperiam, minima saepe quibusdam ea minus vero repellat non. Optio temporibus iure quasi blanditiis quas sunt ut mollitia accusantium architecto voluptatem. Dolorem voluptas atque vel?
+ Voluptatem temporibus placeat repellat! Suscipit praesentium asperiores porro repellendus odio? Expedita repellendus minus repudiandae modi facilis totam quam delectus, eius pariatur quis sed nisi ratione dolores recusandae enim ullam impedit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci porro fuga eum, fugit eaque aspernatur eligendi voluptas, velit quisquam corporis voluptatem quidem! Voluptatibus voluptatum, debitis fugit dolorum ex cupiditate distinctio. </p>
+<div className="flex justify-center">     
+   <button className="bg-blue-700 text-white p-5 rounded-full text-2xl hover:bg-blue-950 border-4 border-yellow-500"> {/* Added border */}
+    Click me
+  </button>     
+</div>
+</div>
+)
 }
 
 export default page
